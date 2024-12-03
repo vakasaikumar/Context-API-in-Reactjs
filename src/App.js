5afc869fc14d5aeac735fbd85107aa1d26602ffe
vakasaikumar <105001957+vakasaikumar@users.noreply.github.com>
2024-12-03 11:55:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React,{useState} from "react";
-import axios from "axios";
+import React,{createContext,useState} from 'react'
+import Count from './Components/Count';
+import Display from './Components/Display';
 
-const App = () =>{
-  const[data,setData] = useState({
-    fullname:'',
-    email:'',
-    password:'',
-    confirmpassword:''
-  })
-  const{fullname,email,password,confirmpassword} = data;
-  const changeHandler = ele=>{
-    setData({...data,[ele.target.name]:ele.target.value})
-  }
-  const submitHandler = ele =>{
-    ele.preventDefault();
-    axios.get('https://vskdev-6670a-default-rtdb.firebaseio.com/register.json',
-      data).then(()=> alert('Submited Successfully'))
-    //console.log(data)
-  }
-  return(
-    <div>
+
+export const store = createContext();
+
+const App = () => {
+  const[data,setData] = useState([
+    {
+      brandName:'Realme'
+    },
+    {
+      brandName:'Oppo'
+    },
+    {
+      brandName:'Noika'
+    }
+  ]);
+
+  return (
+    <store.Provider value={[data,setData]}>
       <center>
-        <form onSubmit={submitHandler}> 
-          <input type="text" name="fullname" value={fullname} onChange={changeHandler}/><br/>
-          <input type="email" name="email" value={email} onChange={changeHandler}/><br/>
-          <input type="password" name="password" value={password} onChange={changeHandler}/><br/>
-          <input type="password" name="confirmpassword" value={confirmpassword} onChange={changeHandler}/><br/>
-          <input type="submit" name="submit"/>
-        </form>
+        <Count />
+        <Display />
       </center>
-    </div>
+    </store.Provider>
   )
 }
-export default App;
+
+export default App
